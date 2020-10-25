7baa8cb23d990c4e426bb2c2aadd22162d7eec31
Randall Lowe <safelock@users.noreply.github.com>
2020-10-25 15:02:45 -0700

    Update config.php
    Noting the .gitignore.

--- config.php
+++ config.php
@@ -1,4 +1,11 @@
 <?php
+    # DO NOT UPLOAD ME WITH SECRETS TO ANY PUBLIC REPO!
+    # Secret Keys are used to authenticate with the CAPTCHA services to verify a test.
+    # Keys should not be shared with anyone else.
+    # If you are updating this as a template, ensure that all keys are redacted first.
+    # An example is <SERVICE_NAME_SECRET_KEY_GOES_HERE>
+    # For more instructions, including the command to forcibly update config.php on Git, see .gitignore
+
     ############# DO NOT EDIT THE BELOW SECTION #############
     $__CONFIGFILELOC = __FILE__;
     $__CONFIGFILECALL = $_SERVER['DOCUMENT_ROOT'].$_SERVER['PHP_SELF'];
